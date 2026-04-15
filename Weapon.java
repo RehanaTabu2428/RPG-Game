b97d2ec25f37b673e99092d35dfb298216f422a5
Rehana Tabassum <rehanatabassumjosephite2428@gmail.com>
2026-04-16 03:08:13 +0530

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,15 @@
+package com.rpg.items;
+
+public class Weapon {
+    private String name;
+    private int damage;
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
